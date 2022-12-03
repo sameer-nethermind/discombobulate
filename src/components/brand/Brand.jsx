a39af0898ba5bcbdf38a626c3cebc83eb2fdd281
Sameer Kumar <sameer@nethermind.io>
2022-12-03 23:33:37 +0530

    style..

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import "./brand.css";
+import Button from "@mui/material/Button";
 
 const Brand = () => (
   <div className="gpt3__brand section__padding">
     <div>
-      <h3>One</h3>
+      <h3>
+        <a className="btn" href="/">
+          Lodge Complaint
+        </a>
+      </h3>
     </div>
     <div>
-      <h3>One</h3>
-    </div>
-    <div>
-      <h3>One</h3>
-    </div>
-    <div>
-      <h3>One</h3>
-    </div>
-    <div>
-      <h3>One</h3>
+      <h3>
+        <Button variant="contained">Lodge Complaint</Button>
+      </h3>
     </div>
   </div>
 );
