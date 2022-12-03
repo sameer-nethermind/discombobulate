14547a5ccd136e38f12a590043d95a4b5f17ffd0
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-12-04 01:52:49 +0530

    adfmk

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
 import { useState, React } from "react";
 import Web3Modal from "web3modal";
 import { useRef, useEffect } from "react";
-import { providers } from "ethers";
-import { Button } from "@mui/material";
 import Home from "./pages/Home";
-// import Wrapper from "./pages/Wrapper";
 import WalletConnectProvider from "@walletconnect/web3-provider";
-
 import "./App.css";
 
 function App() {
-  const [currentAccount, setCurrentAccount] = useState(null);
-
-  // const connectWalletHandler = async () => {
-  //   const { ethereum } = window;
-  //   if (!ethereum) {
-  //     alert("Please install Metamask!");
-  //   }
-  //   try {
-  //       const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-  //       console.log("Found an account! Address: ", accounts[0]);
-  //       setCurrentAccount(accounts[0]);
-  //     } catch (err) {
-  //       console.log(err)
-  //   }
-  // }
-
-  const connectWalletButton = () => {
-    return (
-      <button
-        onClick={connectWallet}
-        className="cta-button connect-wallet-button"
-      >
-        Connect Wallet
-      </button>
-    );
-  };
-
   const [web3Modal, setWeb3Modal] = useState(null);
-
   useEffect(() => {
-    // initiate web3modal
     const providerOptions = {
       walletconnect: {
         package: WalletConnectProvider,
@@ -49,19 +16,13 @@ function App() {
         },
       },
     };
-
     const newWeb3Modal = new Web3Modal({
-      cacheProvider: true, // very important
+      cacheProvider: true, 
       network: "mainnet",
       providerOptions,
     });
-
     setWeb3Modal(newWeb3Modal);
   }, []);
-
-  async function connectWallet() {
-    const provider = await web3Modal.connect();
-  }
 
   return (
     <div className="App">
