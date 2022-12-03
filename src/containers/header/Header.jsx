c76baddcb2fafbcc928bf55b4e965c484d45b787
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-12-04 01:14:27 +0530

    adsf

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import people from '../../assets/people.png';
-import ai from '../../assets/logo.jpeg';
+import ai from '../../assets/logo.png';
 import './header.css';
 
 const Header = () => (
